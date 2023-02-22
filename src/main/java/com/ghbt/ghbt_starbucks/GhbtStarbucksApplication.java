70b90be2eb72c324494a94eba308536738ca41ee
nam <namgh627@naver.com>
2023-02-22 13:27:19 +0900

    intellij init

---
+++ src/main/java/com/ghbt/ghbt_starbucks/GhbtStarbucksApplication.java
@@ -0,0 +1,13 @@
+package com.ghbt.ghbt_starbucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhbtStarbucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhbtStarbucksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghbt/ghbt_starbucks/GhbtStarbucksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghbt.ghbt_starbucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhbtStarbucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
