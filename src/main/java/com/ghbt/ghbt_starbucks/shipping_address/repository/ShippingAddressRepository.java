20b07f2afb9c8b6366826c25bcb4f599d4137a33
geunskoo <oryukdo3@pusan.ac.kr>
2023-02-27 17:35:27 +0900

    rename : shippingaddress 구조 이름 변경

--- src/main/java/com/ghbt/ghbt_starbucks/shipping_address/repository/ShippingAddressRepository.java
+++ src/main/java/com/ghbt/ghbt_starbucks/shipping_address/repository/ShippingAddressRepository.java
@@ -1,6 +1,6 @@
-package com.ghbt.ghbt_starbucks.shippingaddress.repository;
+package com.ghbt.ghbt_starbucks.shipping_address.repository;
 
-import com.ghbt.ghbt_starbucks.shippingaddress.model.ShippingAddress;
+import com.ghbt.ghbt_starbucks.shipping_address.model.ShippingAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShippingAddressRepository extends JpaRepository<ShippingAddress, Long> {
