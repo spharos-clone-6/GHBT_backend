69a9368d5694750c79249a7af6a0e817192b4fef
geunskoo <oryukdo3@pusan.ac.kr>
2023-02-27 10:16:43 +0900

    feat: User 회원가입을 위한 static 메서드 작성

--- src/main/java/com/ghbt/ghbt_starbucks/user/model/User.java
+++ src/main/java/com/ghbt/ghbt_starbucks/user/model/User.java
@@ -1,9 +1,12 @@
 package com.ghbt.ghbt_starbucks.user.model;
 
+import com.ghbt.ghbt_starbucks.security.dto.SignupDto;
 import com.ghbt.ghbt_starbucks.shippingaddress.model.ShippingAddress;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,9 +14,11 @@ import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Builder
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
@@ -26,19 +31,22 @@ public class User {
   @OneToMany
   private List<ShippingAddress> shippingAddressList;
 
-  @Column(name = "name", nullable = false, length = 255)
+  @Column(name = "name", nullable = false)
   private String name;
 
-  @Column(name = "user_uuid", nullable = false, length = 255)
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+  @Column(name = "user_uuid", nullable = false)
   private String userUUID;
 
-  @Column(name = "password", nullable = false, length = 255)
+  @Column(name = "password", nullable = false)
   private String password;
 
-  @Column(name = "phone_number", nullable = false, length = 255)
+  @Column(name = "phone_number", nullable = false)
   private String phoneNumber;
 
-  @Column(name = "email", nullable = false, length = 255)
+  @Column(name = "email", nullable = false)
   private String email;
 
   @Column(name = "is_agree", nullable = false)
@@ -61,4 +69,12 @@ public class User {
 
   @Column(name = "business_recipe")
   private String businessRecipe;
+
+  public static User signinUser(SignupDto signupDto) {
+    return new UserBuilder()
+        .email(signupDto.getEmail())
+        .password(signupDto.getPassword())
+        .role(Role.USER)
+        .build();
+  }
 }
