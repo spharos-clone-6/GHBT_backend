a3425f491f47c745b0a63315b75e0498af533c52
geunskoo <oryukdo3@pusan.ac.kr>
2023-02-27 17:36:45 +0900

    rename : ShippingAddressRepository -> IShippingAddressRepository 로 이름 변경

--- src/main/java/com/ghbt/ghbt_starbucks/shipping_address/repository/IShippingAddressRepository.java
+++ src/main/java/com/ghbt/ghbt_starbucks/shipping_address/repository/IShippingAddressRepository.java
@@ -3,6 +3,6 @@ package com.ghbt.ghbt_starbucks.shipping_address.repository;
 import com.ghbt.ghbt_starbucks.shipping_address.model.ShippingAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ShippingAddressRepository extends JpaRepository<ShippingAddress, Long> {
+public interface IShippingAddressRepository extends JpaRepository<ShippingAddress, Long> {
 
 }
