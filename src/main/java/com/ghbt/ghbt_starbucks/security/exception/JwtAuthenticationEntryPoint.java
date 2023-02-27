a83649ea9c3789fc68ff6342c7dabe92feab9ec5
geunskoo <oryukdo3@pusan.ac.kr>
2023-02-27 11:20:27 +0900

    feat: 예외처리 코드를 작성하였습니다.

---
+++ src/main/java/com/ghbt/ghbt_starbucks/security/exception/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,19 @@
+package com.ghbt.ghbt_starbucks.security.exception;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+  @Override
+  public void commence(HttpServletRequest request, HttpServletResponse response,
+      AuthenticationException authException) throws IOException, ServletException {
+    response.setCharacterEncoding("utf-8");
+    response.sendError(401, "잘못된 접근입니다.");
+
+  }
+}
