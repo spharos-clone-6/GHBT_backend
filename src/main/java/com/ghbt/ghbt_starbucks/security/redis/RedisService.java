6d784a2f7d39faefe538891da6200c107e2b8914
geunskoo <oryukdo3@pusan.ac.kr>
2023-02-27 11:19:35 +0900

    feat: Redis 서비스와 저장소 세팅

---
+++ src/main/java/com/ghbt/ghbt_starbucks/security/redis/RedisService.java
@@ -0,0 +1,35 @@
+package com.ghbt.ghbt_starbucks.security.redis;
+
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class RedisService {
+
+  private final RedisTemplate<String, String> redisTemplate;
+
+  @Transactional
+  public void setValues(String key, String value) {
+    redisTemplate.opsForValue().set(key, value);
+  }
+
+  @Transactional
+  public void setValuesWithTimeout(String key, String value, long timeout) {
+    redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.MILLISECONDS);
+  }
+
+  public String getValues(String key) {
+    return redisTemplate.opsForValue().get(key);
+  }
+
+  @Transactional
+  public void deleteValues(String key) {
+    redisTemplate.delete(key);
+  }
+
+}
