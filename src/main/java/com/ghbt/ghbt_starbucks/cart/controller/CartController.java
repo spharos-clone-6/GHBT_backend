9733eaa8dd3772ec327a6385046311b729bc2d5d
nam <namgh627@naver.com>
2023-02-27 17:36:16 +0900

    feat/cart CartController.java addCart

---
+++ src/main/java/com/ghbt/ghbt_starbucks/cart/controller/CartController.java
@@ -0,0 +1,23 @@
+package com.ghbt.ghbt_starbucks.cart.controller;
+
+
+import com.ghbt.ghbt_starbucks.cart.service.ICartService;
+import com.ghbt.ghbt_starbucks.cart.vo.RequestCart;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cart")
+@RequiredArgsConstructor
+@Slf4j
+@CrossOrigin(origins = "http://localhost:3000")
+public class CartController {
+
+    private final ICartService iCartService;
+
+    @PostMapping("/")
+    public void addCart(@RequestBody RequestCart requestCart){
+        iCartService.addCart(requestCart);
+    }
+}
