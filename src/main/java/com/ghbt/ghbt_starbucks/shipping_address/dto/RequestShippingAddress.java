f13f9e00577ecc41994e3e3513d3af751e12a8a1
geunskoo <oryukdo3@pusan.ac.kr>
2023-02-28 17:17:51 +0900

    feat : shippingAdressDto를 작성하였습니다.

---
+++ src/main/java/com/ghbt/ghbt_starbucks/shipping_address/dto/RequestShippingAddress.java
@@ -0,0 +1,53 @@
+package com.ghbt.ghbt_starbucks.shipping_address.dto;
+
+import com.ghbt.ghbt_starbucks.shipping_address.model.ShippingAddress;
+import com.ghbt.ghbt_starbucks.user.model.User;
+import javax.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestShippingAddress {
+
+  @NotEmpty
+  private String receiver;
+
+  @NotEmpty
+  private String zipCode;
+
+  @NotEmpty
+  private String addressNickname;
+
+  @NotEmpty
+  private String detailAddress;
+
+  @NotEmpty
+  private String baseAddress;
+
+  @NotEmpty
+  private String phoneNumber1;
+
+  private String phoneNumber2;
+
+  private String notice;
+
+  private Boolean isDefault;
+
+  public ShippingAddress toEntity(RequestShippingAddress requestShippingAddress, User user) {
+    return ShippingAddress.builder()
+        .user(user)
+        .receiver(requestShippingAddress.getReceiver())
+        .zipCode(requestShippingAddress.getZipCode())
+        .addressNickname(requestShippingAddress.getAddressNickname())
+        .detailAddress(requestShippingAddress.getDetailAddress())
+        .baseAddress(requestShippingAddress.getBaseAddress())
+        .phoneNumber1(requestShippingAddress.getPhoneNumber1())
+        .phoneNumber2(requestShippingAddress.getPhoneNumber2())
+        .notice(requestShippingAddress.getNotice())
+        .isDefault(requestShippingAddress.getIsDefault())
+        .build();
+  }
+}
