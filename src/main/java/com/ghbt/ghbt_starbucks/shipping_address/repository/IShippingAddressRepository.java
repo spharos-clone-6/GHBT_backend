88dd8292731cc64952e2a5d11adc7797ee40c39d
geunskoo <oryukdo3@pusan.ac.kr>
2023-02-28 15:30:54 +0900

    feat : shippingAddress crud api 개발을 완료하였습니다. rest api를 지향하기 위해서 추가 수정 필요합니다.

--- src/main/java/com/ghbt/ghbt_starbucks/shipping_address/repository/IShippingAddressRepository.java
+++ src/main/java/com/ghbt/ghbt_starbucks/shipping_address/repository/IShippingAddressRepository.java
@@ -1,8 +1,13 @@
 package com.ghbt.ghbt_starbucks.shipping_address.repository;
 
 import com.ghbt.ghbt_starbucks.shipping_address.model.ShippingAddress;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IShippingAddressRepository extends JpaRepository<ShippingAddress, Long> {
 
+  Optional<ShippingAddress> findByUserIdAndIsDefault(Long userId, Boolean isDefault);
+
+  List<ShippingAddress> findAllByUserId(Long userId);
 }
---
+++ src/main/java/com/ghbt/ghbt_starbucks/shipping_address/service/IShippingAddressService.java
@@ -0,0 +1,25 @@
+package com.ghbt.ghbt_starbucks.shipping_address.service;
+
+import com.ghbt.ghbt_starbucks.shipping_address.dto.RequestShippingAddress;
+import com.ghbt.ghbt_starbucks.shipping_address.dto.ResponseShippingAddress;
+import com.ghbt.ghbt_starbucks.user.model.User;
+import java.util.List;
+
+public interface IShippingAddressService {
+
+  //배송지 1건 저장.
+  Long saveShippingAddress(RequestShippingAddress requestShippingAddress, User user);
+
+  //기본 배송지 조회.
+  ResponseShippingAddress getDefaultShippingAddress(User user);
+
+  //모든 배송지 조회.
+  List<ResponseShippingAddress> getAllShippingAddress(User user);
+
+  //배송지 삭제
+  void deleteShippingAddress(Long ShippingAddressId);
+
+  //배송지 수정
+  Long updateShippingAddress(RequestShippingAddress requestUpdateShippingAddress,
+      Long ShippingAddressId);
+}
