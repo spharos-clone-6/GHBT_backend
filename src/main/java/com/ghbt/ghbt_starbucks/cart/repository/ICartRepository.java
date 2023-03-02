5cb10fe43310554f9584c3bc44a6b113a949c50a
nam <namgh627@naver.com>
2023-03-02 19:41:44 +0900

    feat/cart/#35 레포지토리 findByDeletedId query문 작성

--- src/main/java/com/ghbt/ghbt_starbucks/cart/repository/ICartRepository.java
+++ src/main/java/com/ghbt/ghbt_starbucks/cart/repository/ICartRepository.java
@@ -2,9 +2,15 @@ package com.ghbt.ghbt_starbucks.cart.repository;
 
 import com.ghbt.ghbt_starbucks.cart.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ICartRepository extends JpaRepository<Cart, Long> {
     List<Cart> findAllByUser_Id(Long userid);
+
+    @Query( value = "SELECT id from cart where deleted = TRUE",nativeQuery = true)
+    Cart findByDeletedId(Long id);
+
+    Cart updateByDeletedColumn(Long id);
 }
