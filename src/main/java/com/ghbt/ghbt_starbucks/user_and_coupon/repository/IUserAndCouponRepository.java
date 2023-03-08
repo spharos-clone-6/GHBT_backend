9f05e3b2992b9fb9a76323098bdf6e34a33a15c4
ShinHyeonchae <72970253+scsok13@users.noreply.github.com>
2023-03-08 15:59:20 +0900

    rename/coupon/#69 : couponList 패키지 user_and_coupon으로 변경 및 하단의 클래스도 양식에 맞추어 이름변경

---
+++ src/main/java/com/ghbt/ghbt_starbucks/user_and_coupon/repository/IUserAndCouponRepository.java
@@ -0,0 +1,7 @@
+package com.ghbt.ghbt_starbucks.user_and_coupon.repository;
+
+import com.ghbt.ghbt_starbucks.user_and_coupon.model.UserAndCoupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserAndCouponRepository extends JpaRepository<UserAndCoupon, Long> {
+}
