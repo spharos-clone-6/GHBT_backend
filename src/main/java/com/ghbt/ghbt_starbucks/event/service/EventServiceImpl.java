28b024e7ebe8575d769785caee95ddc0355d5d99
nam <namgh627@naver.com>
2023-03-08 16:13:37 +0900

    feat/event/#69 : addEvent

--- src/main/java/com/ghbt/ghbt_starbucks/event/service/EventServiceImpl.java
+++ src/main/java/com/ghbt/ghbt_starbucks/event/service/EventServiceImpl.java
@@ -1,4 +1,38 @@
 package com.ghbt.ghbt_starbucks.event.service;
 
-public class EventServiceImpl {
+import com.ghbt.ghbt_starbucks.event.dto.RequestEvent;
+import com.ghbt.ghbt_starbucks.event.dto.ResponseEvent;
+import com.ghbt.ghbt_starbucks.event.model.Event;
+import com.ghbt.ghbt_starbucks.event.repository.IEventRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Data
+@RequiredArgsConstructor
+public class EventServiceImpl implements IEventService{
+
+   private final IEventRepository iEventRepository;
+
+    @Override
+    public void addEvent(RequestEvent requestEvent) {
+        Event event = Event.builder()
+                .description(requestEvent.getDescription())
+                .descriptionUrl(requestEvent.getDescriptionUrl())
+                .thumbnailUrl(requestEvent.getThumbnailUrl())
+                .name(requestEvent.getName())
+                .build();
+        iEventRepository.save(event);
+    }
+
+    @Override
+    public ResponseEvent getEventById(Long Id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEvent getEventByName(String name) {
+        return null;
+    }
 }
