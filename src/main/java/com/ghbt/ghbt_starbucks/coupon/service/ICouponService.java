63f5f9fd8e6c0fba57f6548c8cd36b7527c57b8e
ShinHyeonchae <72970253+scsok13@users.noreply.github.com>
2023-03-08 15:54:59 +0900

    remove/coupon/#69 : coupon의 불필요한 서비스 레포지토리 삭제

--- src/main/java/com/ghbt/ghbt_starbucks/coupon/service/ICouponService.java
+++
@@ -1,9 +0,0 @@
-package com.ghbt.ghbt_starbucks.coupon.service;
-
-import com.ghbt.ghbt_starbucks.coupon.dto.RequestCoupon;
-import com.ghbt.ghbt_starbucks.coupon.dto.ResponseCoupon;
-
-public interface ICouponService {
-    ResponseCoupon addCoupon(RequestCoupon requestCoupon);
-
-}
