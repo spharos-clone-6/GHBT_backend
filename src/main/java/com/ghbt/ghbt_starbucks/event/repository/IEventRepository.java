ee9cba76761eb0f1fadd3855a57fb833cccc519b
nam <namgh627@naver.com>
2023-03-08 14:26:39 +0900

    feat/event/#69 : ieventrepository 작성

---
+++ src/main/java/com/ghbt/ghbt_starbucks/event/repository/IEventRepository.java
@@ -0,0 +1,7 @@
+package com.ghbt.ghbt_starbucks.event.repository;
+
+import com.ghbt.ghbt_starbucks.event.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IEventRepository extends JpaRepository<Event,Long> {
+}
