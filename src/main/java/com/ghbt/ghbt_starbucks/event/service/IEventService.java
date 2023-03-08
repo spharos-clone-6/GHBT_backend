079453ad6f945929deae82b7bf95b9d2b640baa9
nam <namgh627@naver.com>
2023-03-08 17:56:35 +0900

    fix/event/#69 : git pull 후 다시 올리겠습니다.

---
+++ src/main/java/com/ghbt/ghbt_starbucks/event/service/IEventService.java
@@ -0,0 +1,14 @@
+package com.ghbt.ghbt_starbucks.event.service;
+
+import com.ghbt.ghbt_starbucks.event.dto.RequestEvent;
+import com.ghbt.ghbt_starbucks.event.dto.ResponseEvent;
+
+public interface IEventService {
+
+
+    void addEvent(RequestEvent requestEvent);
+    ResponseEvent getEventById(Long id);
+
+    ResponseEvent getEventByName(String name);
+
+}
