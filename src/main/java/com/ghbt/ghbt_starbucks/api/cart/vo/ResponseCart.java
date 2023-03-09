2f8636974d209748b0304e315d7088a99720b089
nam <namgh627@naver.com>
2023-03-09 17:08:22 +0900

    fix/cart/#85 : formating

--- src/main/java/com/ghbt/ghbt_starbucks/api/cart/vo/ResponseCart.java
+++ src/main/java/com/ghbt/ghbt_starbucks/api/cart/vo/ResponseCart.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResponseCart {
+
     private Integer quantity;
     private User user;
     private Product product;
