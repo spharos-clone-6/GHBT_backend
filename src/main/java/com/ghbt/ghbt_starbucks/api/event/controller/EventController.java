f33374aebf67afadc89223d8e8c307cd672ad7a8
nam <namgh627@naver.com>
2023-03-14 13:57:59 +0900

    feat/event : controller  getEventAll 작성

--- src/main/java/com/ghbt/ghbt_starbucks/api/event/controller/EventController.java
+++ src/main/java/com/ghbt/ghbt_starbucks/api/event/controller/EventController.java
@@ -3,7 +3,9 @@ package com.ghbt.ghbt_starbucks.api.event.controller;
 import com.ghbt.ghbt_starbucks.api.event.dto.ResponseEvent;
 import com.ghbt.ghbt_starbucks.api.event.service.IEventService;
 import com.ghbt.ghbt_starbucks.api.event.dto.RequestEvent;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -19,19 +21,26 @@ public class EventController {
     private final IEventService iEventService;
 
     @PostMapping()
+    @Operation(summary = "이벤트 작성", description = "requestbody에 입력해 주세요")
     public void addEvent(@RequestBody RequestEvent requestEvent) {
         iEventService.addEvent(requestEvent);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "이벤트 id로 검색", description = "파라미터에 id 입력해주세요.")
     public ResponseEvent getEventById(@PathVariable Long id) {
         return iEventService.getEventById(id);
     }
 
     @GetMapping("/name/{name}")
+    @Operation(summary = "이벤트 이름으로 검색", description = "파라미터에 name 입력해주세요.")
     public ResponseEvent getEventByName(@PathVariable String name) {
         return iEventService.getEventByName(name);
     }
 
-
+    @GetMapping("/name/{name}")
+    @Operation(summary = "이벤트 리스트 전체 검색", description = "이벤트 리스트 전체 검색합니다.")
+    public List<ResponseEvent> getEventAll(){
+        return iEventService.getEventAll();
+    }
 }
