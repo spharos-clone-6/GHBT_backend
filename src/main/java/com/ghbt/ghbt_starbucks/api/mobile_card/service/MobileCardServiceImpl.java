9042ef7da88b33da2907295242a3d952250299fe
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-03-19 15:14:39 +0900

    feat/mobile_card/#135 : mobile card 등록 과정시 모바일 카드 번호에 따른 모바일 카드 종류를 매칭시키는 알고리즘을 추가하였습니다.

--- src/main/java/com/ghbt/ghbt_starbucks/api/mobile_card/service/MobileCardServiceImpl.java
+++ src/main/java/com/ghbt/ghbt_starbucks/api/mobile_card/service/MobileCardServiceImpl.java
@@ -2,14 +2,17 @@ package com.ghbt.ghbt_starbucks.api.mobile_card.service;
 
 import static com.ghbt.ghbt_starbucks.global.error.ErrorCode.*;
 
-import com.ghbt.ghbt_starbucks.api.mobile_card.dto.RequestMobileCardToEnroll;
+import com.ghbt.ghbt_starbucks.api.mobile_card.dto.RequestEnrollMobileCard;
 import com.ghbt.ghbt_starbucks.api.mobile_card.dto.ResponseMobileCard;
+import com.ghbt.ghbt_starbucks.api.mobile_card.model.CardType;
 import com.ghbt.ghbt_starbucks.api.mobile_card.model.MobileCard;
 import com.ghbt.ghbt_starbucks.api.mobile_card.repository.MobileCardRepository;
 import com.ghbt.ghbt_starbucks.global.error.ServiceException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,16 +37,32 @@ public class MobileCardServiceImpl implements IMobileCardService {
     @Override
     public ResponseMobileCard getOneMobileCard(Long mobileCardId) {
         MobileCard findMobileCard = mobileCardRepository.findById(mobileCardId)
-            .orElseThrow(() -> new ServiceException(NOT_FOUND_MOBILE_CARD.getMessage(), NOT_FOUND_MOBILE_CARD.getHttpStatus()));
+            .orElseThrow(
+                () -> new ServiceException(NOT_FOUND_MOBILE_CARD.getMessage(), NOT_FOUND_MOBILE_CARD.getHttpStatus()));
 
         return ResponseMobileCard.from(findMobileCard);
     }
 
     @Override
     @Transactional
-    public Long enrollMobileCard(RequestMobileCardToEnroll requestMobileCardToEnroll) {
-        MobileCard mobileCard = MobileCard.toEntity(requestMobileCardToEnroll);
+    public Long enrollMobileCard(RequestEnrollMobileCard requestEnrollMobileCard) {
+        CardType cardType = encryptCardType(requestEnrollMobileCard.getCardNumber(),
+            requestEnrollMobileCard.getPinNumber());
+        MobileCard mobileCard = MobileCard.toEntity(requestEnrollMobileCard, cardType);
         MobileCard savedMobileCard = mobileCardRepository.save(mobileCard);
         return savedMobileCard.getId();
+    }
+
+    private CardType encryptCardType(String cardNumber, String pinNumber) {
+        Long cardTypeKey = Long.parseLong(pinNumber) % (
+            Long.parseLong(cardNumber) % 10000
+                + Long.parseLong(cardNumber) / 10000 % 10000
+                + Long.parseLong(cardNumber) / 10000 / 10000 % 10000
+                + Long.parseLong(cardNumber) / 10000 / 10000 / 10000 % 10000
+        ) % 4;
+        return Arrays.stream(CardType.values())
+            .filter(c -> c.getKey().equals(cardTypeKey))
+            .findFirst()
+            .orElseThrow(() -> new ServiceException("잘못된 카드 번호 및 pin 형식 입니다.", HttpStatus.NOT_FOUND));
     }
 }
