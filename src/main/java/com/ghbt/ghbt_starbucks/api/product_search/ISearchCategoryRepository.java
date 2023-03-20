08275d2617c8d448b1ecb8449e2bc91fa797bbb1
Junseokee <102888442+Junseokee@users.noreply.github.com>
2023-03-20 13:52:18 +0900

    refactor/package/product search package add

--- src/main/java/com/ghbt/ghbt_starbucks/api/product_search/ISearchCategoryRepository.java
+++ src/main/java/com/ghbt/ghbt_starbucks/api/product_search/ISearchCategoryRepository.java
@@ -1,6 +1,6 @@
-package com.ghbt.ghbt_starbucks.api.product_and_category.repository;
+package com.ghbt.ghbt_starbucks.api.product_search;
 
-import com.ghbt.ghbt_starbucks.api.product_and_category.model.SearchCategory;
+import com.ghbt.ghbt_starbucks.api.product_search.SearchCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ISearchCategoryRepository extends JpaRepository<SearchCategory, Long> {
