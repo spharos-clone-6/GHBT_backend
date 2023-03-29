3646440263ccbac39227490e837395e8c9e1cf8d
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-03-29 23:47:51 +0900

    fix/purchase/#188 : 결제 승인 도메인을 배포서버로 변경하였습니다.

--- src/main/java/com/ghbt/ghbt_starbucks/api/kakaopay/KakoPayUrl.java
+++ src/main/java/com/ghbt/ghbt_starbucks/api/kakaopay/KakoPayUrl.java
@@ -9,7 +9,7 @@ public enum KakoPayUrl {
     READY_TO_POST("https://kapi.kakao.com/v1/payment/ready"),
     APPROVE_TO_POST("https://kapi.kakao.com/v1/payment/approve"),
     CANCEL("http://localhost:8080/api/payment/cancel"),
-    APPROVAL("http://localhost:8080/api/payment/success"),
+    APPROVAL("http://backend.grapefruit-honey-black-tea.shop/api/payment/success"),
     FAIL("http://localhost:8080/api/payment/fail"),
     ;
 
