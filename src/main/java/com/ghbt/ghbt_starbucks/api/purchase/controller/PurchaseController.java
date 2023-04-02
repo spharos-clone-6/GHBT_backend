19fe1466579b209ab7f759644317af5c809c0fba
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-04-02 23:20:34 +0900

    feat/purchase/#188 : 구매 내역 저장 API와 결제과정 중 취소 상황로직을 구현하였습니다.

--- src/main/java/com/ghbt/ghbt_starbucks/api/purchase/controller/PurchaseController.java
+++ src/main/java/com/ghbt/ghbt_starbucks/api/purchase/controller/PurchaseController.java
@@ -29,16 +29,32 @@ public class PurchaseController {
 
     @Operation(summary = "구매하기(장바구니", description = "장바구니를 통한 구매")
     @PostMapping
-    public ResponseEntity<Object> startPurchases(@RequestBody RequestPurchase requestPurchase, @LoginUser User loginUser) {
+    public ResponseEntity<Object> startPurchase(@RequestBody RequestPurchase requestPurchase,
+        @LoginUser User loginUser) {
         ResponsePayment responseReady = iPurchaseService.startPayment(requestPurchase, loginUser);
         return ResponseEntity.status(HttpStatus.OK)
             .body(responseReady);
+    }
+
+    @PostMapping("/end")
+    public ResponseEntity<?> endPurchase(@LoginUser User loginUser) {
+        iPurchaseService.endPayment(loginUser);
+        return ResponseEntity.status(HttpStatus.OK)
+            .build();
+    }
+
+    @PostMapping("/cancel")
+    public ResponseEntity<?> cancelPurchase(@LoginUser User loginUser) {
+        iPurchaseService.cancelPurchase(loginUser);
+        return ResponseEntity.status(HttpStatus.OK)
+            .build();
     }
 
     @Operation(summary = "구매내역 조회(단건)", description = "purchaseId를 long 으로 입력해주세요")
     @Parameters({@Parameter(name = "purchaseId", description = "구매내역 고유 번호", example = "1")})
     @GetMapping("/{purchaseId}")
     public ResponsePurchase getPurchaseById(@PathVariable Long purchaseId) {
+
         return iPurchaseService.getPurchaseById(purchaseId);
     }
 
